076f4b332507465bb532b78d6f5f524bc6db52c7
Maximiliano Eschoyez <meschoyez@gmail.com>
2021-08-20 16:42:47 -0300

    Algunos Ejercicios

---
+++ src/main/haskell/Ejercicios1A.hs
@@ -0,0 +1,28 @@
+-- A
+{- 
+Escriba una funcion recursiva que calcule el resto de la
+division entera utilizando solamente restas.
+ -}
+
+{- 
+Escriba una funcion recursiva que calcule la sumatoria de los
+enteros entre a y b:
+   sumarAB a b = a + (a+1) + (a+2) + ... + (b-1) + b
+ -}
+
+{- 
+Escriba una funcion que calcule la suma de las cifras de un
+entero:
+   sumaCifras 123 = 6
+ -}
+
+{- 
+Dada la representacion de fechas como tupla (day, month, year),
+definir la funcion showFecha :: (Int, Int, Int) -> [Char].
+Ejemplos: 
+   showFecha (10, 12, 2013) = "10 de Diciembre de 2013"
+   showFecha (21,  0, 2021) = "21 de Enero de 2021"
+Recordar que Int es de la clase Show, y se puede usar la
+instruccion show para convertir a string.
+ -}
+
---
+++ src/main/haskell/Ejercicos1B.hs
@@ -0,0 +1,29 @@
+-- B
+{- 
+Escriba una funcion recursiva que calcule el cociente de la
+division entera utilizando solamente sumas y restas.
+ -}
+
+{-
+Escriba una funcion recursiva que calcule el producto de los
+enteros entre a y b:
+   prodAB a b = a * (a+1) * (a+2) * ... * (b-1) * b
+ -}
+
+{- 
+Escriba una funcion que calcule la cantidad de cifras de un
+entero:
+   cantCifras 123 = 3
+ -}
+
+{- 
+Dada la representacion de fechas como tupla (day, month, year),
+definir la funcion showFecha :: (Int, Int, Int) -> [Char].
+Ejemplos: 
+   showFecha (10, 12, 2013) = "10 de Diciembre de 2013"
+   showFecha (21,  0, 2021) = "21 de Enero de 2021"
+Recordar que Int es de la clase Show, y se puede usar la
+instruccion show para convertir a string.
+ -}
+
+
